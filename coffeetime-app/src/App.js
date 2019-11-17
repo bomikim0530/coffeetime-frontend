281593755b22563097dfd0401c3d8a278a532d02
Chintan Vyas <chintanvyas360@gmail.com>
2019-11-17 12:45:18 -0800

    Authentication plus cleaning up
    - Added Authentication
    - Added icon
    - Removed other images and icon
    - Added react boostrap for UI and other dependencies
    - Added sample react bootstrap code

--- coffeetime-app/src/App.js
+++ coffeetime-app/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// React imports
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Amplify imports
+import { configureAmplify } from "./services";
+import Auth from "@aws-amplify/auth";
+import { withAuthenticator } from "aws-amplify-react";
+
+// Start: Styling imports
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+// Start: React bootstrap imports
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// End: React bootstrap imports
+// End: Styling imports
+
+class App extends Component {
+  state = {
+    identityId: ""
+  };
+
+  constructor(props) {
+    super(props);
+    Auth.currentCredentials().then(
+      response => {
+        // console.log(response.data.IdentityId);
+        this.state.identityId = response.data.IdentityId;
+      },
+      err => {
+        console.log("Unable to fetch current credentials");
+      }
+    );
+  }
+
+  render() {
+    return (
+      <Container className="App">
+        <Row>
+          <Col>Welcome to CoffeeTime powered with Reaact Bootstrap!</Col>
+          <Col>
+            <Button variant="primary">React Boostrap Button</Button>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default App;
+configureAmplify();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+export default withAuthenticator(App, true);
