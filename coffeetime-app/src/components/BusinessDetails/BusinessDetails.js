af0f5fb39883d8c24ff205d23e90c3ff1ac99d64
admin <admin@admins-Air-2.attlocal.net>
2019-11-27 22:25:34 -0800

    first

--- coffeetime-app/src/components/BusinessDetails/BusinessDetails.js
+++ coffeetime-app/src/components/BusinessDetails/BusinessDetails.js
@@ -2,13 +2,39 @@
 import React, { Component } from "react";
 // Start: Styling imports
 import "./BusinessDetails.css";
+// Other components import
+import CommentForm from './CommentForm.js';
+import CommentView from './CommentView.js';
+import Favorite from './Favorite.js';
+
 class BusinessDetails extends Component {
   render() {
     return (
       <div>
-        Welcome to business details!!
-
-        BOMII!!
+      <br/>
+      <h1>Restaurant Name</h1><br/>
+      <table>
+      <tbody>
+      <tr>
+          <td width="400px">Image from Yelp</td>
+          <td width="700px">
+          <br/>
+          <h3>Details</h3>
+          <table className="inside">
+            <tbody>
+              <tr>A little table with the details about the vendor : this is to see how long it can get</tr>
+            </tbody>
+          </table><br/>
+          <h3>Reviews</h3>
+          <div><table className="inside"><CommentView pageid = "BusinessDetails"/></table></div><br/>
+          <h3>Leave Reviews</h3>
+          <div><CommentForm pageid = "BusinessDetails"/> </div><br/>
+          <h3>Save as Favorite</h3>
+          <div><Favorite pageid = "BusinessDetails"/> </div><br/>
+          </td>
+      </tr>
+      </tbody>
+      </table>
       </div>
     );
   }
---
+++ coffeetime-app/src/components/BusinessDetails/Favorite.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Checkbox from "./Checkbox";
+
+const OPTIONS = ["Check if it's your favorite!"];
+
+class Favorite extends React.Component {
+  state = {
+    checkboxes: OPTIONS.reduce(
+      (options, option) => ({
+        ...options,
+        [option]: false
+      }),
+      {}
+    )
+  };
+
+  handleCheckboxChange = changeEvent => {
+    const { name } = changeEvent.target;
+
+    this.setState(prevState => ({
+      checkboxes: {
+        ...prevState.checkboxes,
+        [name]: !prevState.checkboxes[name]
+      }
+    }));
+  };
+
+  handleFormSubmit = formSubmitEvent => {
+    formSubmitEvent.preventDefault();
+
+    Object.keys(this.state.checkboxes)
+      .filter(checkbox => this.state.checkboxes[checkbox])
+      .forEach(checkbox => {
+        console.log(checkbox, "is selected.");
+      });
+    localStorage.setItem("Test Person", {"This Restaurant":"True"});
+  };
+
+  createCheckbox = option => (
+    <Checkbox
+      label={option}
+      isSelected={this.state.checkboxes[option]}
+      onCheckboxChange={this.handleCheckboxChange}
+      key={option}
+    />
+  );
+
+  createCheckboxes = () => OPTIONS.map(this.createCheckbox);
+
+  render() {
+    return (
+      <div>
+            <form onSubmit={this.handleFormSubmit}>
+            {this.createCheckboxes()}<input type="submit" value="Save" className="btn btn-dark"/>
+            </form>
+      </div>
+    );
+  }
+}
+
+export default Favorite;
